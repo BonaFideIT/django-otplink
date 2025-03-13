d5075421ed346542b3b7cda877e49e7fc42ee0d0
Oguzhan Kaluk <oguzhan.kaluk@gmail.com>
2025-03-13 13:22:34 +0100

    #1 cleanup

--- src/otplink/functions.py
+++ src/otplink/functions.py
@@ -1,4 +1,5 @@
-from datetime import datetime, timedelta
+from datetime import timedelta
+from django.utils import timezone
 from django.http.response import Http404
 from .models import OtpObject
 
@@ -20,11 +21,11 @@ def create_otp_link(instance, file_field: str, quantity: int=1, duration: int=24
 
 def retrieve_otp_link_instance(otp_object: OtpObject):
     # check if instance can be retrieved
-    if otp_object.quantity < 1:
-        raise Http404
-    if datetime.now() > otp_object.created_at + timedelta(hours=otp_object.duration):
-        raise Http404
-    if otp_object.content_object is None:
+    if (
+        otp_object.quantity < 1 # usage maximum is exceeded
+        or timezone.now() > otp_object.created_at + timedelta(hours=otp_object.duration) # duration is exceeded
+        or otp_object.content_object is None # instance is deleted
+    ):
         raise Http404
 
     # decrement quantity
