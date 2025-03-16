a038d21d4c79e4267882372312b11fb024628ba2
Oguzhan Kaluk <oguzhan.kaluk@gmail.com>
2025-03-16 15:37:44 +0100

    #1 readme updates. extend to have all available optional fields

--- src/otplink/functions.py
+++ src/otplink/functions.py
@@ -4,17 +4,28 @@ from django.http.response import Http404
 from .models import OtpObject
 
 
-def create_otp_link(instance, file_field: str, quantity: int=1, duration: int=24):
-    # check if filefield exists in the instancemodel
+def create_otp_link(
+        instance, file_field: str, quantity: int=1, duration: int=24,
+        basename_field: str=None, encoding_field: str=None,
+        mime_type_field: str=None, charset_field: str=None,
+        modification_time_field: str=None, size_field: str=None,
+) -> OtpObject:
+    # check if file_field exists in the instancemodel
     if not hasattr(instance, file_field):
         raise AttributeError(f'{instance.__class__.__name__} has no attribute {file_field}')
 
     # create the otp link
     otp = OtpObject.objects.create(
-        content_object=instance,
-        file_field=file_field,
         quantity=quantity,
         duration=duration,
+        content_object=instance,
+        file_field=file_field,
+        basename_field=basename_field,
+        encoding_field=encoding_field,
+        mime_type_field=mime_type_field,
+        charset_field=charset_field,
+        modification_time_field=modification_time_field,
+        size_field=size_field,
     )
 
     return otp
--- src/otplink/views.py
+++ src/otplink/views.py
@@ -14,8 +14,13 @@ class OTPDownloadView(ObjectDownloadView):
         obj = super().get_object(queryset)
 
         # overwrite ObjectDownloadView attributes from OtpObject
-        # optional: extend with other attributes configurable in ObjectDownloadView
         self.file_field = obj.file_field
+        self.basename_field = obj.basename_field
+        self.encoding_field = obj.encoding_field
+        self.mime_type_field = obj.mime_type_field
+        self.charset_field = obj.charset_field
+        self.modification_time_field = obj.modification_time_field
+        self.size_field = obj.size_field
 
         # return instance of model specified in OtpObject
         return retrieve_otp_link_instance(obj)
